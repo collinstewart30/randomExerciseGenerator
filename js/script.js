ce7c5eeb4506db64ebdd611ea3125c1e90393d0e
Collin Stewart <collin.stewart30@yahoo.com>
2021-05-21 00:11:53 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*!
+* Start Bootstrap - Business Frontpage v5.0.0 (https://startbootstrap.com/template/business-frontpage)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+function reloadPage() {
+    window.location.href = 'http://localhost/exerciseprog/index.php#workout';
+    window.location.reload(true);
+}
